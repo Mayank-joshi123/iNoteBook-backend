6dfcdab7202ab855c4874b028599c3db10ad904a
Mayank-Joshi01 <mayankjoshi.in.123@gmail.com>
2025-01-05 22:35:08 +0530

    CORS

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const mongoURI = process.env.MONGO_URI
-
+console.log(mongoURI)
 const connectToMongodb = ()=>{
     mongoose.connect(mongoURI)
     console.log("Connected to Mongooes")
--- index.js
+++ index.js
@@ -15,7 +15,11 @@ app.use(express.json())
 app.use(express.json({limit: '100mb'}));
 app.use(express.urlencoded({limit: '100mb', extended: true}));
 
-app.use(cors());
+app.use(cors((x => x
+  .AllowAnyMethod()
+  .AllowAnyHeader()
+  .SetIsOriginAllowed(origin => true) // allow any origin
+  .AllowCredentials())));
 
 // Avilable routes 
 app.use("/api/auth",require("./routes/auth.js"));
